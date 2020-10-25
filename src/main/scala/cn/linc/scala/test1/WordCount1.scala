397e7186456433922e2bafcf3704579c06590900
xiechenglin <1303380546@qq.com>
2020-10-25 12:54:49 +0800

    spark init

---
+++ src/main/scala/cn/linc/scala/test1/WordCount1.scala
@@ -0,0 +1,22 @@
+package cn.linc.scala.test1
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+
+object WordCount1 {
+
+  def main(args: Array[String]): Unit = {
+    // 1. 创建 Spark Context
+    val conf = new SparkConf().setMaster("local[2]").setAppName("wordCountTest")
+    val sc: SparkContext = new SparkContext(conf)
+    // 2. 读取文件并计算词频
+    val source: RDD[String] = sc.textFile("src/main/dataset/wordcount.txt", 2)
+    val words: RDD[String] = source.flatMap { line => line.split(" ") }
+    val wordsTuple: RDD[(String, Int)] = words.map { word => (word, 1) }
+    val wordsCount: RDD[(String, Int)] = wordsTuple.reduceByKey { (x, y) => x + y }
+    // 3. 查看执行结果
+    val result = wordsCount.collect;
+    result.foreach(item => println(item))
+  }
+
+}
